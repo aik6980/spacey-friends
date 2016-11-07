fe64665822f9e7f33eacfb8574184fece52701a8
Thomas Benyon <tom.benyon@gmail.com>
2016-11-07 14:44:44 +0000

    Add basic keyboard input for rotation

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,15 +12,52 @@ var GameState = {
         this.ship = this.game.add.sprite(250, 250, 'ship');
         this.ship.anchor.setTo(0.5);
         this.ship.scale.setTo(0.1);
+        this.ship.rotationSpeed = 3;
+        this.ship.direction = 0;
 
         this.ufo = this.game.add.sprite(250, 50, 'ufo');
         this.ufo.anchor.setTo(0.5);
         this.ufo.scale.setTo(0.1);
+
+
+
+        this.right = game.input.keyboard.addKey(Phaser.Keyboard.RIGHT);
+        this.right.onDown.add(rotateShipRight, this);
+        this.right.onUp.add(stopRotateRight, this);
+
+        this.left = game.input.keyboard.addKey(Phaser.Keyboard.LEFT);
+        this.left.onDown.add(rotateShipLeft, this);
+        this.left.onUp.add(stopRotateLeft, this);
+
+
+
     },
     update: function () {
-        this.ship.angle += 1;
+        this.ship.angle += this.ship.direction * this.ship.rotationSpeed;
     }
+
 };
+
+function rotateShipRight() {
+    this.ship.direction = 1;
+}
+
+function stopRotateRight() {
+    if (this.ship.direction > 0) {
+        this.ship.direction = 0;
+    }
+}
+
+function rotateShipLeft() {
+    this.ship.direction = -1;
+}
+
+function stopRotateLeft() {
+    if (this.ship.direction < 0) {
+        this.ship.direction = 0;
+    }
+}
+
 
 //Add all states
 game.state.add("GameState", GameState);
