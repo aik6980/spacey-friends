f011a14d3c1c57e4dc448528cccd6056089d53c8
Thomas Benyon <tom.benyon@gmail.com>
2016-11-08 21:28:56 +0000

    Add basic socket.io integration

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 var express = require('express');
+var http = require('http');
 var app = express();
+var server = http.createServer(app);
+var io = require('socket.io').listen(server);
 
 app.set('view engine', 'jade');
 app.set('views', __dirname + '/assets/views');
@@ -16,9 +19,16 @@ app.get('/controller', function(req, res){
 
 var port = process.env.PORT || 3000;
 
-var server = app.listen(port, function () {
+var server = server.listen(port, function () {
     var host = server.address().address;
     var port = server.address().port;
 
     console.log('App listening at http://%s:%s', host, port);
+});
+
+io.on('connection', function (socket) {
+    socket.emit('news', { hello: 'world' });
+    socket.on('my other event', function (data) {
+        console.log(data);
+    });
 });
