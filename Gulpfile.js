e11670419890a71aebf0a8d738d7ba76f0bf927d
aik6980 <aik6980>
2016-12-29 18:37:39 +0000

    generating sourcemaps for using in debugger add VSCode launch file for code debugging support

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,5 +1,15 @@
+/*
+Note: 
+ [29/12/2016] ".pipe(sourcemaps.write( ".", { sourceRoot : "../../game"} ))"
+	- "../../game", our sourcemap file is generated next to .js file, 
+	however, we have to set the correct value for sourceRoot so VSCode can find the path of the original cource code (for example "game.ts")
+	I'm not sure about other tools (IntelliJ) but this helps resolving the correct path for the current .vscode/launch.json
+	(VSCode uses this file to attach/launch debugger)
+*/
+
 var gulp = require("gulp");
 var ts = require("gulp-typescript");
+var sourcemaps = require("gulp-sourcemaps");
 var concat = require('gulp-concat');
 
 // typescript project for game
@@ -28,13 +38,17 @@ gulp.task("js_move", function () {
 gulp.task("typescript", function () {
 	var output_folder = "public/js";
 	var ts_result_game 		= ts_project_game.src()
+							.pipe(sourcemaps.init())
 							.pipe(ts_project_game())
 							.pipe(concat(ts_project_game_output))
+							.pipe(sourcemaps.write( ".", { sourceRoot : "../../game"} ))
 							.pipe(gulp.dest(output_folder));
 							
 	var ts_result_controller = ts_project_controller.src()
+							.pipe(sourcemaps.init())
 							.pipe(ts_project_controller())
 							.pipe(concat(ts_project_controller_output))
+							.pipe(sourcemaps.write( ".", { sourceRoot : "../../controller"} ))
 							.pipe(gulp.dest(output_folder));
     return ts_result_game && ts_result_controller;
 });
